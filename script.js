f5494de149ab3a56fc30939fcc4ae8e7bab890db
Saif Mahammed <fivestromcode@gmail.com>
2023-12-25 12:44:31 +0530

    Updated

---
+++ script.js
@@ -0,0 +1,22 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 100;
+        this.height = 100;
+    }
+    draw() {
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+const player = new Player();
+player.draw();
